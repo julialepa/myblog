666677c8b1acc44387abc6c44066ce62dda53d9f
roberto <roberto@hyperting.com>
2018-12-12 10:27:54 +0100

    refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
-import Home from './components/home/home'
-import Post from './components/post/post'
-import Modal from './components/usermodal/modal'
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./components/home/home";
+import Post from "./components/post/post";
+import Modal from "./components/usermodal/modal";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       post: [],
-    }
-    this.dataFetch();
+      loader: false
+    };
+    // this.dataFetch();
   }
 
+  componentDidMount() {
+    this.setState({ loader: true });
+    this.dataFetch();
+    this.setState({ loader: false });
+  }
 
   dataFetch() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
-      .then(json => this.setState({
-        post: json
-      })
-      )
+      .then(json =>
+        this.setState({
+          post: json
+        })
+      );
   }
 
-
-
   render() {
+    const {loader, post} = this.state
     return (
       <div>
         <Home />
-        <div className="carousel-container">
-        </div>
-        <div className="feed-page">
-          {this.state.post.map(post => <Post title={post.title} body={post.body} />)}
-        </div>
+        {loader ? (
+          <p>'...loading'</p>
+        ) : (
+          <div className="carousel-container">
+            <div className="feed-page">
+              {post.map(item => (
+                <Post title={item.title} body={item.body} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
